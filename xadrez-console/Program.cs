fcb5edf08784ba308760eb398efb493ae594d808
Linconl <linconl_eu@outlook.com>
2022-01-30 15:48:58 -0300

    Implementado conversão de posicao do tabueliro de xadrez; Implementado cores e melhora na impressao da tela;

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -13,8 +13,12 @@ namespace xadrez_console
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(0, 0));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(4, 5));
+
 
                 Tela.imprimirTabuleiro(tab);
+
+               
             }
             catch (TabuleiroException e)
             {
---
+++ xadrez-console/xadrez/PosicaoXadrez.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace xadrez
+{
+    class PosicaoXadrez
+    {
+        public char coluna { get; set; }
+        public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+        public Posicao toPosicao()//coverte a posicao de xadrez, para posicao de matriz
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+            //lembrando que linha e coluna comecam em 0.
+            //se a posicao de xadrez for C7(coluna C, linha 7 [debaixo pra cima]), será em matriz igual a 1,2(linha 8-7 = 1; coluna 'C' - 'A' = 2 [3-1 = 2])
+
+        }
+        public override string ToString()
+        {
+            return $"{coluna}{linha}";
+        }
+    }
+}
